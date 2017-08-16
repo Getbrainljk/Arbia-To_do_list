c417cb7737a0b4359ecb83ce94d92f3ef2e491fe
Nadir Arbia <nadir.arbia@gmail.com>
2017-08-16 22:21:43 +0200

    up

---
+++ Arbia-To_do_list/App.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Todolist
+{
+	public class App : Form
+	{
+		public App ()
+		{
+			Text = "To Do List ";
+			Size = new Size (800, 500);
+			CenterToScreen ();
+			Task task = new Task ();
+		}
+		protected override bool ProcessCmdKey(ref Message msg, Keys keyData) {
+			if (keyData == Keys.Escape) {
+				this.Close();
+				return true;
+			}
+			return base.ProcessCmdKey(ref msg, keyData);
+		}
+
+		static void Main()
+		{
+			Application.Run(new App());
+		}
+	}
+
+}
+
+
---
+++ Arbia-To_do_list/Task.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Todolist
+{
+	public class Task : App
+	{
+		public Task ()
+		{
+			add ();	
+		}
+		// add task
+		public void add()
+		{
+			Button addtask = new Button ();
+			addtask.Text = " + ";
+			addtask.Width = 60;
+			addtask.Height = 50; 
+			addtask.Left = 15;
+			addtask.Top = 10;
+			this.Controls.Add (addtask);
+			addtask.ImageAlign = ContentAlignment.MiddleRight;    
+			addtask.TextAlign = ContentAlignment.MiddleLeft;
+			addtask.FlatStyle = FlatStyle.Flat;
+			addtask.Font = new Font (addtask.Font.FontFamily, 25);
+		}
+		public void edit()
+			{}
+		public void consult()
+			{}
+		public void delete()
+			{}
+	}	
+}
+
